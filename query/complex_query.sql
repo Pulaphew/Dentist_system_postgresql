ae01244a57faaf6845d6e772e60d671e08e307ca
Takinsen <tandevill90@gmail.com>
2025-02-03 12:35:48 +0700

    back-up

--- query/complex_query.sql
+++ query/complex_query.sql
@@ -1,3 +1,5 @@
+-- แสดงชื่อ-นามสุกล เงินเดือน ของหมอฟันที่มีเงินเดือนมากกว่าค่าเฉลี่ยของหมอฟันทุกคนในคลินิกสาขาตัวเอง
+
 SELECT first_name AS Dentist_First_Name , last_name AS Dentist_Last_Name, base_salary AS Salary
 FROM user_account NATURAL JOIN dentist_account NATURAL JOIN (
 	SELECT clinic_id , AVG(base_salary) AS avg_salary
--- query/simple_query.sql
+++ query/simple_query.sql
@@ -1,3 +1,5 @@
+-- แสดงเบอร์โทรหมอฟัน และ รหัสคลินิก โดยที่เงินเดือนหมอฟันมากกว่า 50000 บาท
+
 SELECT tel , clinic_id
 FROM dentist_account
 WHERE base_salary > 50000
